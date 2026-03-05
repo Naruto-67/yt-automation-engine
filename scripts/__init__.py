0896d1c4bc77e2c487a760cc8aa8b5296debb05d
Naruto <tomvsjerry67@gmail.com>
2026-03-05 13:51:21 +0530

    Update __init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,2 +1,3 @@
 # Ghost Engine V4.0 - Package Initialization
 # This file is intentionally blank to allow module-based imports.
+# It ensures that 'scripts' can be found as a package from the root directory.
